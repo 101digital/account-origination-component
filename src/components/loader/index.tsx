741521e1afb76ce68a1fea5916b34a420b985329
sharmal33 <sharmal@101digital.io>
2022-05-12 11:35:33 +0530

    Version Update

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,41 +1,43 @@
-import { colors, fonts } from '../../assets';
-import { LoaderIcon } from '../../assets/icons';
-import React,{useEffect} from 'react';
-import { SafeAreaView, StyleSheet, Text,ActivityIndicator } from 'react-native';
-
+import { colors, fonts } from "../../assets";
+import { LoaderIcon } from "../../assets/icons";
+import React, { useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  ActivityIndicator
+} from "react-native";
 
 interface ComponentProps {
-    onExacute: () => void;
+  onExacute: () => void;
 }
 
 export type LoaderProps = ComponentProps;
 
 const Loader = (prop: LoaderProps) => {
-  let count = 0
+  let count = 0;
 
-  useEffect(()=>{
-    count = 0
-  },[])
+  useEffect(() => {
+    count = 0;
+  }, []);
 
-  useEffect(()=>{
-
-    var handle=setInterval(
-      () => {
+  useEffect(() => {
+    var handle = setInterval(() => {
       count++;
-      prop.onExacute(count)},10000);
+      prop.onExacute(count);
+    }, 10000);
 
-    return ()=>{
+    return () => {
       clearInterval(handle);
-    }
+    };
   });
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Hang on for a moment</Text>
       <Text style={styles.subTitle}>We’re currently setting things up.</Text>
-      <LoaderIcon width={60} height={60} />
       <ActivityIndicator
-        size={'large'}
+        size={"large"}
         style={styles.loadingIndicatorStyle}
         color={colors.primary}
       />
@@ -47,9 +49,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center"
   },
   title: {
     fontFamily: fonts.bold,
@@ -58,21 +60,21 @@ const styles = StyleSheet.create({
     color: colors.primary,
     paddingHorizontal: 24,
     marginBottom: 32,
-    textAlign: 'center',
+    textAlign: "center"
   },
   subTitle: {
     fontFamily: fonts.bold,
     fontSize: 16,
     lineHeight: 24,
-    color: '#7F7B82',
+    color: "#7F7B82",
     paddingHorizontal: 24,
     marginBottom: 32,
-    textAlign: 'center',
-    paddingBottom: 100,
+    textAlign: "center",
+    paddingBottom: 100
   },
   loadingIndicatorStyle: {
-      marginTop: 80,
-    },
+    marginTop: 80
+  }
 });
 
 export default Loader;
